d73b36109f83ba322b4d55c205c201ca3c256591
jack ogina <jackogina60@gmail.com>
2018-08-18 07:33:49 +0300

    source & article structs

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+type Sources struct {
+	Sources []Source `json: "sources"`
+}
+type Source struct {
+	Id          string `json: "id"`
+	Name        string `json: "name"`
+	Description string `json: "description"`
+	Category    string `json: "category"`
+	Url         string `json: "url"`
+	Language    string `json: "language"`
+	Country     string `json: "country"`
+}
+
+type Articles struct {
+	Articles     []Article `json: "articles"`
+	TotalResults int       `json: "totalReults"`
+}
+type Article struct {
+	Author      string `json: "author"`
+	Title       string `json: "title"`
+	Description string `json: "description"`
+	Url         string `json: "url"`
+	UrlToImage  string `json: "urlToImage"`
+	PublishedAt string `json: "publishedAt"`
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+	createServer()
+
+}
